77208973ae12c39e48a68106af15f584b89132bb
Vitor Aguiar <joaovitor4guiar@gmail.com>
2023-10-06 09:03:46 -0300

    feat: add generic repository interface

---
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Contracts.Persistence;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    Task<T> GetByIdAsync(int id);
+    Task<T> GetAllAsync();
+    Task<T> CreateAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(T entity);
+}
+
+public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
+{
+
+}
