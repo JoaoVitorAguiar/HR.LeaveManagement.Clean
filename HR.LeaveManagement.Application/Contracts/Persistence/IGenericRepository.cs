0214ff94c6f4dda14998d5c1c4fb7f905ddb1b67
Vitor Aguiar <joaovitor4guiar@gmail.com>
2023-10-09 14:09:01 -0300

    fix: change return in GetAll

--- HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -9,7 +9,7 @@ namespace HR.LeaveManagement.Application.Contracts.Persistence;
 
 public interface IGenericRepository<T> where T : BaseEntity
 {
-    Task<T> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetByIdAsync(int id);
     Task<T> GetAllAsync();
     Task<T> CreateAsync(T entity);
     Task<T> UpdateAsync(T entity);
