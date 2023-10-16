bd1bc39a9693727f10c06e5e4dce5dab699d8593
Vitor Aguiar <joaovitor4guiar@gmail.com>
2023-10-16 17:28:36 -0300

    feat: add BadRequestException

---
+++ HR.LeaveManagement.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation.Results;
+
+namespace HR.LeaveManagement.Application.Exceptions;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException(string messege) : base(messege)
+    {
+        ValidationErros = new List<string>();
+    }
+
+    public BadRequestException(string messege, ValidationResult validationResult) : base(messege)
+    {
+        ValidationErros = new List<string>();
+        foreach(var error in validationResult.Errors)
+        {
+            ValidationErros.Add(error.ErrorMessage);
+        }
+    }
+
+    public List<string> ValidationErros { get; set; }
+}
+
+
+
