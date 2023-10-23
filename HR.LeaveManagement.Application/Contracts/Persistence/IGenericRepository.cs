0425baea21bc06832b1cf1046b279fde0289359a
Vitor Aguiar <joaovitor4guiar@gmail.com>
2023-10-23 11:09:36 -0300

    fix: refactor the interface GenericRepository

--- HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -10,8 +10,8 @@ namespace HR.LeaveManagement.Application.Contracts.Persistence;
 public interface IGenericRepository<T> where T : BaseEntity
 {
     Task<T> GetByIdAsync(int id);
-    Task<IEnumerable<T>> GetAllAsync();
-    Task<T> CreateAsync(T entity);
-    Task<T> UpdateAsync(T entity);
-    Task<T> DeleteAsync(T entity);
+    Task<IReadOnlyCollection<T>> GetAllAsync();
+    Task<bool> CreateAsync(T entity);
+    Task<bool> UpdateAsync(T entity);
+    Task<bool> DeleteAsync(T entity);
 }
