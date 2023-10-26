c9224bb739849e939e334c34799df9890fafe2df
Vitor Aguiar <joaovitor4guiar@gmail.com>
2023-10-26 07:38:19 -0300

    feat: add service Email

--- HR.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
+++ HR.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Models.Email;
+using HR.LeaveManagement.Infrastructure.EmailService;
+using Microsoft.Extensions.Options;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace HR.LeaveManagement.Infrastructure;
 
@@ -13,6 +12,9 @@ public static class InfrastructureServicesRegistration
     public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, 
         IConfiguration configuration)
     {
+        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailSender, EmailSender>();
+       
         return services;
     }
 }
